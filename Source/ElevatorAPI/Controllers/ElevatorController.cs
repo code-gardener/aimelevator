b396d812e8fc2f89b62d231e408e7cc42874292d
Scott Sharpe <v-scshar@microsoft.com>
2021-05-23 16:01:33 -0700

    Core Functionality

--- Source/ElevatorAPI/Controllers/ElevatorController.cs
+++ Source/ElevatorAPI/Controllers/ElevatorController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevatorAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +14,104 @@ namespace ElevatorAPI.Controllers
     [ApiController]
     public class ElevatorController : ControllerBase
     {
-        // GET: api/<ElevatorController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public Car car = new Car() { Id = 1, DropOff = new List<int>(), Pickup = new List<int>() };
+
+        public ElevatorController()
         {
-            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/<ElevatorController>
+        [HttpGet("allRequestedFloors")]
+        public IEnumerable<int> AllRequestedFloors()
+        {
+            List<int> allFloors = new List<int>();
+            allFloors.AddRange(car.Pickup);
+            allFloors.AddRange(car.DropOff);
+            allFloors.Sort();
+
+            return allFloors;
         }
 
         // GET api/<ElevatorController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("getNextFloor")]
+        public int GetNextFloor()
         {
-            return "value";
+            int nextFloor = -1;
+            int nextPickupFloor = -1;
+            int nextDropOffFloor = -1;
+
+            if (car.DropOff.Count == 0 && car.Pickup.Count == 0)
+            {
+                return nextFloor;  // nothing to service.
+            }
+
+            if (car.Direction == 0)
+            {
+                // Get the Pickup Floor <= Current Floor
+                if (car.Pickup.Count > 0)
+                {
+                    nextPickupFloor = car.Pickup.Select(x => x).Where(x => x < car.CurrentFloor).FirstOrDefault();
+                }
+                // Get the DropOff Floor >= Current Floor
+                if (car.DropOff.Count > 0)
+                {
+                    nextDropOffFloor = car.Pickup.Select(x => x).Where(x => x > car.CurrentFloor).FirstOrDefault();
+                }
+            }
+            else
+            {
+                // Get the Pickup Floor ?= Current Floor
+                if (car.Pickup.Count > 0)
+                {
+                    nextPickupFloor = car.Pickup.Select(x => x).Where(x => x > car.CurrentFloor).FirstOrDefault();
+                }
+                // Get the DropOff Floor <= Current Floor
+                if (car.DropOff.Count > 0)
+                {
+                    nextDropOffFloor = car.Pickup.Select(x => x).Where(x => x < car.CurrentFloor).FirstOrDefault();
+                }
+            }
+
+            if (nextDropOffFloor == -1)
+            {
+                return nextPickupFloor;
+            }
+            else if (nextPickupFloor == -1)
+            {
+                return nextDropOffFloor;
+            }
+            else
+            {
+                return Math.Min(nextPickupFloor, nextDropOffFloor);
+            }
         }
 
-        // POST api/<ElevatorController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        // GET api/<ElevatorController>/requestElevator?floor=5
+        [HttpGet("requestElevator")]
+        public int RequestElevator([FromQuery] int floor)
         {
+            car.Pickup.Add(floor);
+            return car.CurrentFloor;
         }
 
-        // PUT api/<ElevatorController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        // GET api/<ElevatorController>/sendElevator?destinationFloor=5
+        [HttpGet("sendElevator")]
+        public int SendElevator([FromQuery] int destinationFloor)
         {
+            car.DropOff.Add(destinationFloor);
+            return car.CurrentFloor;
         }
 
-        // DELETE api/<ElevatorController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // GET api/<ElevatorController>/?moveElevator?direction=15
+        [HttpGet("moveElevator")]
+        public int MoveElevator([FromQuery] int direction)
         {
+            car.Direction = Convert.ToInt32(direction);
+            car.CurrentFloor = this.GetNextFloor();
+            car.DropOff.Remove(car.CurrentFloor);
+            car.Pickup.Remove(car.CurrentFloor);
+
+            return car.CurrentFloor;
         }
     }
 }
---
+++ Source/ElevatorAPI/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevatorAPI.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public int CurrentFloor { get; set; }
+        public List<int> Pickup { get; set; }
+        public List<int> DropOff { get; set; }
+        public int Direction { get; set; }
+    }
+}
