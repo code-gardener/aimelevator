f73a67b67ce97ea6ddeab6958aa629235f8d39c0
Scott Sharpe <v-scshar@microsoft.com>
2021-05-23 14:33:31 -0700

    Initial Commit

---
+++ Tests/ElevatorAPI.Tests/ElevatorControllerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ElevatorAPI.Tests
+{
+    [TestClass]
+    public class ElevatorControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
